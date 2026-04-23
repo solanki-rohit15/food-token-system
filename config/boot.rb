807a6f8935d32f5155fa288ad6a9840dac6444c6
Rohit Solanki <rohitsolanki5005@gmail.com>
2026-04-23 12:21:01 +0530

    Fix: inject secret_key_base at earliest boot point for Render

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,10 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+# config/boot.rb  (add at the bottom)
+if ENV["RAILS_ENV"] == "production" &&
+   ENV["SECRET_KEY_BASE"].blank? &&
+   ENV["RAILS_MASTER_KEY"].blank?
+
+  ENV["SECRET_KEY_BASE"] = "0" * 128   # valid 64-byte dummy
+end
